507074a14534094c3f56e5b65bcce7a95e796ead
李勇 <leonpa1987@gmail.com>
2020-05-09 00:04:07 +0800

    Delete a .c file and a .h file

--- unio_gpio_tuner.c
+++
@@ -1,3 +0,0 @@
-/* File: unio_gpio_tuner.c */
-/* Author: YOU NAME */
-/* Date: Fri Nov 22 10:46:12 CST 2019 */
--- unio_gpio_tuner.h
+++
@@ -1,8 +0,0 @@
-/* File: unio_gpio_tuner.h */
-/* Author: YOU NAME */
-/* Date: Fri Nov 22 10:46:15 CST 2019 */
-#ifndef _UNIO_GPIO_TUNER_H_
-#define _UNIO_GPIO_TUNER_H_
-
-
-#endif
